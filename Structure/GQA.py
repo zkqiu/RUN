c7955999afda3c67d1577c5d903ac64ef9912558
Allen Chiu <zhikangqiu@gmail.com>
2024-12-08 16:44:32 +0800

    add

--- Structure/GQA.py
+++ Structure/GQA.py
@@ -1,6 +1,18 @@
 ## 分组注意力查询
 import torch
 from torch import nn
+
+class FFN(nn.Module):
+    def __init__(self, hidden_size):
+        super(FFN, self).__init__()
+        self.fc1 = nn.Linear(hidden_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.ln = nn.LayerNorm(hidden_size)
+    
+    def forward(self, x):
+        return self.ln(x+self.fc2(self.relu(self.fc1(x))))
+    
 class GroupQueryAttention(torch.nn.Module):
     def __init__(self, hidden_size, num_heads, group_num):
         super(GroupQueryAttention, self).__init__()
@@ -15,7 +27,10 @@ class GroupQueryAttention(torch.nn.Module):
         
         ## 输出线性层
         self.o_linear = nn.Linear(hidden_size, hidden_size)
+        self.ln = nn.LayerNorm(hidden_size)
         
+        self.ffn = FFN(hidden_size)
+
     def forward(self, hidden_state, attention_mask=None):
         batch_size = hidden_state.size()[0]
         
@@ -40,7 +55,9 @@ class GroupQueryAttention(torch.nn.Module):
         
         output = output.transpose(-1, -2).contiguous().view(batch_size, -1, self.head_dim * self.num_heads)
         
-        output = self.o_linear(output)
+        output = self.ln(hidden_state + self.o_linear(output))
+
+        output = self.ffn(output)
         
         return output
         
@@ -54,3 +71,20 @@ class GroupQueryAttention(torch.nn.Module):
             x = x.view(batch_size, -1, group_num, self.head_dim).transpose(1,2)
             x = x[:, :, None, :, :].expand(batch_size, group_num, self.num_heads // group_num, seq_len, self.head_dim).reshape(batch_size, self.num_heads // group_num * group_num, seq_len, self.head_dim)
             return x
+        
+if __name__ == "__main__":
+    batch_size = 2
+    seq_len = 5
+    hidden_size = 16
+    num_heads = 4
+    num_groups = 2
+
+    mqa = GroupQueryAttention(hidden_size, num_heads, num_groups)
+    x = torch.rand(batch_size, seq_len, hidden_size)
+    # key = torch.rand(batch_size, seq_len, hidden_size)
+    # value = torch.rand(batch_size, seq_len, hidden_size)
+    mask = None
+
+    output, attention = mqa(x, mask)
+    print("Output shape:", output.shape)  # Expected: (batch_size, seq_len, hidden_size)
+    print("Attention shape:", attention.shape)  # Expected: (batch_size, num_heads, seq_len, seq_len)
